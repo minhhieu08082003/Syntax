430ccd47ae352132801ce7cbd75af9c7510ae54c
minhhieu08082003 <tranminhhieu08082003@gmail.com>
2021-11-15 11:45:57 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,16 @@
+package basic.dev;
+
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctr + /
+	/*Comment 1 dòng cách 2*/
+	public static void main(String[] args) {
+	System.out.println("Hello World");
+	int number;
+	String nameSchool;
+	}
+	/*
+	* tên phương thức tuân theo quy tắc camelCase
+	*/
+	public void show() {
+	}
+	}
